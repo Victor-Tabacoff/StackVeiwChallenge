a04c0c93a97e35f5e798567dc875514ef28bdec2
Paul Hennig <paul.hennig@d214.org>
2024-08-23 08:26:46 -0500

    Initial Commit

---
+++ StackVeiwChallenge/StackVeiwChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackVeiwChallengeApp.swift
+//  StackVeiwChallenge
+//
+//  Created by Victor B. Tabacoff on 8/23/24.
+//
+
+import SwiftUI
+
+@main
+struct StackVeiwChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
